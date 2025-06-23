c626ccbbd907fc1527d628e23e7634656d5a47ad
suyeon <paksy0428@gmail.com>
2025-06-24 00:53:06 +0900

    feat: 테마 만들기

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,5 +1,5 @@
 import "styled-components";
-import type { Theme, ThemeName } from "./style/theme";
+import type { Theme, ThemeName, ColorKey } from "./style/theme";
 
 declare module "styled-components" {
   export interface DefaultTheme extends Theme {
